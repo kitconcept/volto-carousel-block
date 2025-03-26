0a78be8c1976bf91fb2a48176229772d854d4b66
Jonas Piterek <piterek@kitconcept.com>
2025-03-26 17:15:19 +0100

    chore: automatically create default data

--- packages/volto-carousel-block/src/components/Edit.jsx
+++ packages/volto-carousel-block/src/components/Edit.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { SidebarPortal } from '@plone/volto/components';
+import { v4 as uuid } from 'uuid';
 
 import Sidebar from './Data';
 import View from './View';
 
 const Edit = (props) => {
-  const { selected } = props;
+  const { onChangeBlock, block, selected } = props;
+  // create default data so `data.columns` is never undefined.
+  const data = {
+    columns: [1, 2, 3, 4].map(() => ({ '@id': uuid() })),
+    items_to_show: 4,
+    ...props.data,
+  };
+  if (!props.data.columns) {
+    onChangeBlock(block, data);
+  }
 
   return (
     <>
       <View {...props} isEditMode />
       <SidebarPortal selected={selected}>
-        <Sidebar {...props} />
+        <Sidebar
+          {...props}
+          data={data}
+          block={block}
+          onChangeBlock={onChangeBlock}
+        />
       </SidebarPortal>
     </>
   );
