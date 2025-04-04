c6d400cce2919b38aff0d39efabec728439f985d
Victor Fernandez de Alba <sneridagh@gmail.com>
2025-04-04 13:09:49 +0200

    Added VLT as part of the add-on developer build

--- volto.config.js
+++ volto.config.js
@@ -1,5 +1,8 @@
-const addons = ['@kitconcept/volto-carousel-block'];
-const theme = '';
+const addons = [
+  '@kitconcept/volto-light-theme',
+  '@kitconcept/volto-carousel-block',
+];
+const theme = '@kitconcept/volto-light-theme';
 
 module.exports = {
   addons,
