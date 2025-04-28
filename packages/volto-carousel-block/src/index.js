0bad2f0ac88d4682f93a6d9e4d0cba112649f250
Jonas Piterek <piterek@kitconcept.com>
2025-04-28 11:14:24 +0200

    fix: linting and remove block specific settings

--- packages/volto-carousel-block/src/index.js
+++ packages/volto-carousel-block/src/index.js
@@ -1,22 +1,12 @@
-import { useIntl, defineMessages } from 'react-intl';
 import CarouselEdit from '@kitconcept/volto-carousel-block/components/Edit';
 import CarouselView from '@kitconcept/volto-carousel-block/components/View';
 import { CarouselSchema } from '@kitconcept/volto-carousel-block/components/schema';
 import sliderSVG from '@plone/volto/icons/slider.svg';
 
-const messages = defineMessages({
-  Carousel: {
-    id: 'Carousel',
-    defaultMessage: 'Carousel',
-  },
-});
-
 const applyConfig = (config) => {
-  const intl = useIntl();
-
   config.blocks.blocksConfig.carousel = {
     id: 'carousel',
-    title: intl.formatMessage(messages.Carousel),
+    title: 'Carousel',
     group: 'common',
     icon: sliderSVG,
     view: CarouselView,
@@ -25,8 +15,6 @@ const applyConfig = (config) => {
     mostUsed: true,
     sidebarTab: 1,
     blockSchema: CarouselSchema,
-    // block settings
-    allowLoop: true,
   };
 
   return config;
